fcf61f9734ea1efcb559b92a35afec8808c16084
G3-RA <blessingakp22@gmail.com>
2024-11-20 03:05:56 +0100

    Angular Task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UserProfileCard';
+
+  name: string = 'Amie Adams';
+  age: number = 28;
+  profilePicture: string = '../assets/profilePhoto.jpg';
+  email: string = 'aaammie_adamss@gmail.com';
+
+  //Input binding
+  // profileInput () { }
+
+//Input Binding: Extend the task by allowing the UserProfileCard component to receive a user object from a parent component using @Input (to practice input property binding).
+
 }
